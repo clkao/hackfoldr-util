81e0c2db5edd85ff194aeeed0c25ee748cb7286f
Chia-liang Kao <clkao@clkao.org>
2016-07-23 23:53:39 +0800

    properly bundle for browser

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import babel from 'rollup-plugin-babel'
 import babelrc from 'babelrc-rollup'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
+import builtins from 'rollup-plugin-node-builtins'
 import json from 'rollup-plugin-json'
-import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-js';
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-js'
 
 const pkg = require('./package.json')
 const external = Object.keys(pkg.dependencies)
@@ -12,9 +13,12 @@ const bundled = process.env.BROWSER === 'true'
 const min = process.env.BROWSER === 'true'
 
 const plugins = [
+  builtins(),
   nodeResolve({
     jsnext: true,
-    main: true
+    main: true,
+    builtins: true,
+    browser: bundled
   }),
   json({}),
   commonjs({
@@ -32,7 +36,7 @@ export default {
   external: bundled ? [] : external,
   targets: [
     {
-      dest: bundled ? 'dist/browser.js' : pkg['main'],
+      dest: bundled ? pkg['browser'] : pkg['main'],
       format: 'umd',
       moduleName: 'hackfoldrUtil',
       sourceMap: true
